d6146a394e7a01ca4c70186c5be2701538f7af05
Adrián <adriii.ru@gmail.com>
2017-05-11 17:10:32 +0200

    Add files via upload

--- EjemploTipos.cpp
+++ EjemploTipos.cpp
@@ -1,99 +1,184 @@
 #include <iostream>
+#include <stdlib.h>
 
 using namespace std;
 
 int main ()
 {
 	char opcion;
-		
+	
+	Menu:
+	system ("CLS");	
 	cout << "Ejemplo de uso de variables en C++\n" << endl;
 	
 	cout << "Algunas de las variables en C++ son:" << endl;
-	cout << "-BOOL: Almacena el 0 o el 1.\n";
+	cout << "-BOOL: Almacena el 1 o el 0.\n";
 	cout << "-CHAR: Almacena caracteres.\n";
-	cout << "-UNSIGNED: Almacena numeros positivos.\n";
+	cout << "-UNSIGNED: Almacena numeros positivos y el 0.\n";
 	cout << "-INT: Amacena números enteros.\n";
 	cout << "-DOUBLE: Almacena números reales.\n";
 	cout << "-STRING: Almacena una cadena de caracteres.\n" << endl;
 
-	cout << "Seleccione una opcion de las letras siguientes:" << endl;
-	cout << "-B: Para ver un ejemplo de variables BOOL.\n";
-	cout << "-C: Para ver un ejemplo de variables CHAR.\n";
-	cout << "-U: Para ver un ejemplo de variables UNSIGNED.\n";
-	cout << "-I: Para ver un ejemplo de variables INT.\n";
-	cout << "-D: Para ver un ejemplo de variables DOUBLE.\n";
-	cout << "-S: Para ver un ejemplo de variables STRING." << endl;
+	cout << "Seleccione una opcion de las letras siguientes para ver un ejemplo:" << endl;
+	cout << "-B: Para variables BOOL.\n";
+	cout << "-C: Para variables CHAR.\n";
+	cout << "-U: Para variables UNSIGNED.\n";
+	cout << "-I: Para variables INT.\n";
+	cout << "-D: Para variables DOUBLE.\n";
+	cout << "-S: Para variables STRING." << endl;
 	
-	cout << "Para salir presione Q.\n" << endl;
+	cout << endl << "Para salir introduzca Q.\n" << endl;
 	
 	Comienzo:
 	cout << "Su opción es: ";
 	cin >> opcion;
+	cout << endl;
 	
 	if (opcion == 'B' or opcion == 'b')
 	{
+		system ("CLS");
 		goto B;
 	}
 	if (opcion == 'C' or opcion == 'c')
 	{
+		system ("CLS");
 		goto C;
 	}
 	if (opcion == 'U' or opcion == 'u')
 	{
+		system ("CLS");
 		goto U;
 	}
 	if (opcion == 'I' or opcion == 'i')
 	{
+		system ("CLS");
 		goto I;
 	}
 	if (opcion == 'D' or opcion == 'd')
 	{
+		system ("CLS");
 		goto D;
 	}
 	if (opcion == 'S' or opcion == 's')
 	{
+		system ("CLS");
 		goto S;
 	}
 	if (opcion == 'Q' or opcion == 'q')
 	{
+		system ("CLS");
 		goto Q;
 	}
 	else
 	{
-		cout << "No ha introducido una opción válida, por favor vuelva a intentar." << endl;
+		cout << "No ha introducido una opción válida, por favor vuelva a intentar.\n" << endl;
 		goto Comienzo;
 	}
 			
-	B:
-	cout << "BOOL: Por lo general utiliza 1 byte de memoria, valores: true o false.\n";
-	cout << endl;
-	goto Comienzo;
+	B:	
+	cout << "Variables del tipo BOOL\n" << endl;
+	cout << "BOOL: Puede contener los valores 1 o 0, TRUE o FALSE.\n" << endl;
+	cout << "Introduzca M para volver al menú principal.\n" << endl;
+	cin >> opcion;
 	
-	C:
-   	cout << "CHAR: Utiliza generalmente 1 byte de memoria, permite almacenar un carácter, valores; 256 caracteres.\n";
-	cout << endl;
-	goto Comienzo;
-				
+	if (opcion == 'M' || opcion == 'm')
+	{
+		system ("CLS");
+		goto Menu;
+	}
+	else
+	{
+		cout << endl << "No ha introducido una opción válida, por favor vuelva a intentar.\n" << endl;
+		goto B;
+	}
+	
+	C:	
+	cout << "Variables del tipo CHAR\n" << endl;
+	cout << "CHAR: Puede contener un solo caracter, sea cual sea el introducido.\n" << endl;
+	cout << "Introduzca M para volver al menú principal.\n" << endl;
+	cin >> opcion;
+	
+	if (opcion == 'M' || opcion == 'm')
+	{
+		system ("CLS");
+		goto Menu;
+	}
+	else
+	{
+		cout << endl << "No ha introducido una opción válida, por favor vuelva a intentar.\n" << endl;
+		goto C;
+	}
+			
 	U:
-	cout << "UNSIGNED: Utiliza generalmente 2 bytes de memoria, valores: de 0 a 65535.\n";
-	cout << endl;
-	goto Comienzo;
-		
+	cout << "Variables del tipo UNSIGNED\n" << endl;
+	cout << "UNSIGNED: Puede contener los números naturales incluyendo el 0.\n" << endl;
+	cout << "Introduzca M para volver al menú principal.\n" << endl;
+	cin >> opcion;
+	
+	if (opcion == 'M' || opcion == 'm')
+	{
+		system ("CLS");
+		goto Menu;
+	}
+	else
+	{
+		cout << endl << "No ha introducido una opción válida, por favor vuelva a intentar.\n" << endl;
+		goto U;
+	}
+	
 	I:
-	cout << "INT: Utiliza generalmente 4 bytes de memoria, valores: de -2147483648 a 2147483647.\n";
-	cout << endl;
-	goto Comienzo;
+	cout << "Variables del tipo INT\n" << endl;
+	cout << "INT: Puede contener valores numéricos enteros, positivos o negativos.\n" << endl;
+	cout << "Introduzca M para volver al menú principal.\n" << endl;
+	cin >> opcion;
+		
+	if (opcion == 'M' || opcion == 'm')
+	{
+		system ("CLS");
+		goto Menu;
+	}
+	else
+	{
+		cout << endl << "No ha introducido una opción válida, por favor vuelva a intentar.\n" << endl;
+		goto I;
+	}
 		
 	D:
-	cout << "DOUBLE: Utiliza generalmente 8 bytes de memoria, valores: de 2.2e-308 a 3.4e-38.\n";
-	cout << endl;
-	goto Comienzo;
+	cout << "Variables del tipo DOUBLE\n" << endl;
+	cout << "DOUBLE: Pueden contener números reales, con la limitación de 8 bytes.\n" << endl;
+	cout << "Introduzca M para volver al menú principal.\n" << endl;
+	cin >> opcion;
+			
+	if (opcion == 'M' || opcion == 'm')
+	{
+		system ("CLS");
+		goto Menu;
+	}
+	else
+	{
+		cout << endl << "No ha introducido una opción válida, por favor vuelva a intentar.\n" << endl;
+		goto D;
+	}
 		
 	S:
-	cout << "STRING: Son cadenas de texto que pueden almacenar la cantidad que uno determine al establecerlas.\n";
-	goto Comienzo;
+	cout << "Variables del tipo STRING\n" << endl;
+	cout << "STRING: Pueden contener cadenas de caratéres.\n" << endl;
+	cout << "Introduzca M para volver al menú principal.\n" << endl;
+	cin >> opcion;
+	
+	if (opcion == 'M' || opcion == 'm')
+	{
+		system ("CLS");
+		goto Menu;
+	}
+	else
+	{
+		cout << endl << "No ha introducido una opción válida, por favor vuelva a intentar.\n" << endl;
+		goto S;
+	}
 	
 	Q:
-	cout << "Saliste." << endl;
+	system ("CLS");
+	cout << "Se ha salido del programa.\n" << endl;
 	system ("PAUSE");
 }
